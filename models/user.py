7ca91b936e6ad5628e7c679b6f76b7a8c044308c
SaadM33 <smaaqili@gmail.com>
2024-03-21 01:16:39 +0000

    user 2

--- models/user.py
+++ models/user.py
@@ -5,19 +5,21 @@ from models.base_model import Base
 
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-
-    password = ''
-    first_name = ''
-    last_name = ''
+    if storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        password = ''
+        first_name = ''
+        last_name = ''
