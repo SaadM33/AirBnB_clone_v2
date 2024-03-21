e48dd3515b1f0b75485741c6197e468a2ae86b7f
SaadM33 <smaaqili@gmail.com>
2024-03-21 00:43:15 +0000

    task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+
+class DBStorage:
+    """Database storage class for SQLAlchemy."""
+
+    __engine = None
+    __session = None
+
+
+    def __init__(self):
+        """Initialize DBStorage."""
+    
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine(f"mysql+mysqldb://{user}.{password}@localhost/{db}", pool_pre_ping=True)
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+
+    def reload(self):
+        """Reload objects from the database."""
+        Base.metadata.create_all(self.__engine)
+
+        Session = scoped_session( sessionmaker(bind=self.__engine, expire_on_commit=False) )
+        self.__session = Session()
+
+    def new(self, obj):
+        """Add object to the current database session."""
+        if obj:
+            self.__session.add(obj)
+
+    def all(self, cls=None):
+        """ Query all classes or specific one by ID"""
+        
+
+    def save(self):
+        """Commit all changes of the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete object from the current database session."""
+        if obj:    
+            self.__session.delete(obj)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    if storage_type == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """getter docuemnt"""
+            from models import storage
+            citiesList = []
+            citiesAll = storage.all(City)
+            for city in citiesAll.values():
+                if city.state_id == self.id:
+                    citiesList.append(city)
+            return citiesList
